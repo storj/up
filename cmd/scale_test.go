308fd6481fafee1bdff7a9dff1c3423fcb9ab450
Márton Elek <elek@apache.org>
2021-10-28 11:21:51 +0200

    fix the scale_test unit test

---
+++ cmd/scale_test.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/compose-spec/compose-go/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestScale(t *testing.T) {
+
+	k := types.ServiceConfig{
+		Name:  "storagenode",
+		Image: "foobar",
+	}
+
+	err := scale(&k, "10")
+	require.NoError(t, err)
+
+	require.Equal(t, uint64(10), *k.Deploy.Replicas)
+}
