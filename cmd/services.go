0618499a3792541ff96a83428692ed5a3612bdbc
Márton Elek <elek@apache.org>
2021-10-28 10:52:53 +0200

    argument size validation for sjr service

--- cmd/services.go
+++ cmd/services.go
@@ -7,8 +7,9 @@ import (
 )
 
 var svcCmd = &cobra.Command{
-	Use:   "services",
+	Use:   "services <group or service names>",
 	Short: "return service names given in args",
+	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		services, err := common.ResolveServices(args)
 		fmt.Println(services)
