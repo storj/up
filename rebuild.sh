582b349a4360ac4272ce6b9ab020b34141c344cd
Márton Elek <elek@apache.org>
2021-11-02 08:42:33 +0100

    remove outdated (pre-magefile) rebuid.sh

--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-docker build -t ghcr.io/elek/storj-build -f build.Dockerfile .
-docker build -t ghcr.io/elek/storj:1.39.6 --build-arg BRANCH=v1.39.6 -f pkg/storj.Dockerfile .
-docker build -t ghcr.io/elek/storj-edge:1.14.3 --build-arg BRANCH=v1.14.3  -f pkg/edge.Dockerfile .
