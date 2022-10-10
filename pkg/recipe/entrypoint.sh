abd16c21101a3eed32bb259ffecb97955f5069be
Márton Elek <elek@apache.org>
2022-10-10 17:50:33 +0200

    entrypoint: make entrypoint more generic
     * It can listen on any port, hostnames are not hardcoded if STORJUP_WAIT_FOR is used.
     * external address of storagenodes are not forced
     * option to copy satellite-api identities to make persisted but predictable identites
    
    Change-Id: I6e6872aeb59d5bf33d9a85b9f1bf7087394fae22

--- pkg/recipe/entrypoint.sh
+++ pkg/recipe/entrypoint.sh
@@ -7,9 +7,20 @@ export STORJ_NODE_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," "
 #Generate identity if missing
 if [ "$STORJ_IDENTITY_DIR" ]; then
   if [ ! -f "$STORJ_IDENTITY_DIR/identity.key" ]; then
-    identity --identity-dir $STORJ_IDENTITY_DIR --difficulty 8 create .
+    if [ "$STORJ_USE_PREDEFINED_IDENTITY" ]; then
+      # use predictable, pre-generated identity
+      mkdir -p $(dirname $STORJ_IDENTITY_DIR)
+      cp -r /var/lib/storj/identities/$STORJ_USE_PREDEFINED_IDENTITY $STORJ_IDENTITY_DIR
+    else
+      identity --identity-dir $STORJ_IDENTITY_DIR --difficulty 8 create .
+    fi
   fi
 fi
+
+for i in ${STORJ_WAIT_FOR//,/ }
+do
+    storj-up util wait-for-port "$i"
+done
 
 if [ "$STORJ_WAIT_FOR_DB" ]; then
   storj-up util wait-for-port cockroach:26257
@@ -24,17 +35,16 @@ fi
 if [ "$STORJ_ROLE" == "satellite-api" ]; then
   mkdir -p /var/lib/storj/.local
 
-  #only migrate first time
-  if [ ! -f "/var/lib/storj/.local/migrated" ]; then
+  #only migrate first time, if automatic migration is not defined
+  if [[ ! -f "/var/lib/storj/.local/migrated" && -z "$STORJ_DATABASE_OPTIONS_MIGRATION_UNSAFE" ]]; then
     satellite run migration --identity-dir $STORJ_IDENTITY_DIR
     touch /var/lib/storj/.local/migrated
-
   fi
 fi
 
 if [ "$STORJ_ROLE" == "storagenode" ]; then
   #Initialize config, required only to have all the dirs created
-  export STORJ_CONTACT_EXTERNAL_ADDRESS=$STORJ_NODE_IP:28967
+  : ${STORJ_CONTACT_EXTERNAL_ADDRESS:=$STORJ_NODE_IP:28967}
   if [ -f "/var/lib/storj/.local/share/storj/storagenode/config.yaml" ]; then
     rm "/var/lib/storj/.local/share/storj/storagenode/config.yaml"
   fi
