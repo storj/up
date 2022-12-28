9c413d368a766c444eb9b5e6d4bc405ffcba5122
Ivan Fraixedes <ivan@fraixed.es>
2022-12-28 12:01:30 +0100

    cmd/help.go: Fix command to list recipes
    Previously there was a command to list the services, but, currently,
    there is one to list recipes. This help message was not updated during
    the command refactoring.
    
    Change-Id: Ie3eccf52e55fffdd38a4475c84bfe0fd489c7c19

--- cmd/help.go
+++ cmd/help.go
@@ -4,4 +4,4 @@
 package cmd
 
 // SelectorHelp is a general note about how selectors supposed to be used.
-const SelectorHelp = "<selector> is a service name (like storagenode) maybe with the index (storagenode1) or a recipe (like minimal or db). Recipes can be printed with `storj-up service`"
+const SelectorHelp = "<selector> is a service name (like storagenode) maybe with the index (storagenode1) or a recipe (like minimal or db). Recipes can be printed with `storj-up recipes`"
