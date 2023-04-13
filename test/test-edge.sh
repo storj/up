b41241cb28d29642d6e008a2f407c138d295e511
dlamarmorgan <damein@storj.io>
2023-04-13 14:45:34 -0700

    test/{test-edge.sh,test.sh}: fix and cleanup tests
    Add the missing port to the edge services integration test.
    Update tests to cleanup on exit.
    
    Change-Id: Ifc1dc4454e7e0985b7fef7da26c64d2ec71fef08

--- test/test-edge.sh
+++ test/test-edge.sh
@@ -3,12 +3,27 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 set -ex
 
+cleanup() {
+  if [ -f "docker-compose.yaml" ]
+  then
+    docker compose down
+  fi
+  rm -rf data download sha256.sum
+  rm -rf docker-compose.yaml
+}
+
+trap cleanup EXIT
+
+go install storj.io/storj-up@latest
+
+export STORJUP_NO_HISTORY=true
+
 storj-up init
 
 docker compose down -v
 docker compose up -d
 
-storj-up health
+storj-up health -d 60
 
 dd if=/dev/random of=data count=10240 bs=1024
 sha256sum data > sha256.sum
@@ -17,7 +32,7 @@ eval $(storj-up credentials -e)
 rclone config create --non-interactive storjdev3 storj access_grant=$UPLINK_ACCESS
 
 # using internal satellite-api address
-eval $(docker-compose exec -T satellite-api storj-up credentials --s3 -e -a http://authservice:8888 -s satellite-api)
+eval $(docker-compose exec -T satellite-api storj-up credentials --s3 -e -a http://authservice:8888 -s satellite-api:7777)
 rclone config create --non-interactive storjdevs3 s3 type=s3 provider=Storj access_key_id=$AWS_ACCESS_KEY_ID secret_access_key=$AWS_SECRET_ACCESS_KEY endpoint=http://localhost:9999
 
 BUCKET=bucket$RANDOM
@@ -29,4 +44,3 @@ rm data
 rclone copy storjdevs3:$BUCKET/data download
 mv download/data ./
 sha256sum -c sha256.sum
-docker compose down -v
--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,19 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 set -ex
 
+cleanup() {
+  if [ -f "docker-compose.yaml" ]
+  then
+    docker compose down
+  fi
+  rm -rf data sha256.sum
+  rm -rf docker-compose.yaml
+}
+
+trap cleanup EXIT
+
+go install storj.io/storj-up@latest
+
 export STORJUP_NO_HISTORY=true
 
 storj-up init minimal,db
@@ -10,7 +23,7 @@ storj-up init minimal,db
 docker compose down -v
 docker compose up -d
 
-storj-up health
+storj-up health -d 60
 dd if=/dev/random of=data count=10240 bs=1024
 sha256sum data > sha256.sum
 
@@ -23,4 +36,3 @@ uplink cp data sj://$BUCKET/data
 rm data
 uplink cp sj://$BUCKET/data data 
 sha256sum -c sha256.sum
-docker compose down
