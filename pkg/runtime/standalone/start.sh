58b85b8d69fb9159a5fda17470785b03ced05810
dlamarmorgan <damein@storj.io>
2023-05-03 13:56:25 -0700

    runtime/standalone/start.sh: Add process cleanup to default scripts
    Change-Id: I11c71316461770116403e6aa91fa9827264ff802

--- pkg/runtime/standalone/start.sh
+++ pkg/runtime/standalone/start.sh
@@ -3,6 +3,12 @@ cd $(dirname "${BASH_SOURCE[0]}")
 
 set -euxo pipefail
 
+cleanup(){
+  kill $(jobs -p)
+  wait $(jobs -p)
+}
+trap cleanup EXIT
+
 {{ range $k,$v := .Service.Environment}}
 export {{$k}}="{{$v}}"{{end}}
 mkdir -p ./{{.Service.ID.Name}}/{{.Service.ID.Instance}}
